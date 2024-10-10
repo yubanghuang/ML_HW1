db8912fd21f0ae9564f331880a4bb1d77f4ce8af
yubanghuang <yubanghuang@gmail.com>
2024-10-10 16:35:36 +0800

    .

--- Test/test.py
+++ Test/test.py
@@ -9,9 +9,3 @@ import pandas as pd
 
 # load mnist dataset
 digits = datasets.load_digits()
-print(digits)
-
-fig ,ax = plt.subplots(nrows=8,ncols=8)
-for i , axi in enumerate(ax.flat) :
-    axi.imshow(digits.images[i],cmap='gray')
-plt.show()
